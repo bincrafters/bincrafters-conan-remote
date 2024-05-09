91bca5d1f50bd7e3adf4a43d498eb21dfa2361b5
Croydon <git@cr0ydon.com>
2024-05-09 05:42:15 +0200

    Generate: Parameterize remote configuration and local port

--- generate.py
+++ generate.py
@@ -10,6 +10,8 @@ import shutil
 import subprocess
 import atexit
 import time
+import argparse
+import urllib.parse
 
 from fastapi import FastAPI, Request
 from fastapi.responses import Response
@@ -37,6 +39,29 @@ async def make_request(getting_url: str, user_agent: str):
 @app.get("/{url_path:path}")
 async def get_external_site(request: Request, url_path: str):
 
+    # Remote Configuration
+    # /r/conan+{url}+name/
+    remote_type = "conan"
+    remote_types_allowed = ["conan",]
+    remote_http_url = "https://bincrafters.jfrog.io/artifactory/api/conan/conan-legacy-inexorgame/"
+    remote_config = "inexorgame"
+
+    if url_path.startswith("r/"):
+        url_path = url_path[len("r/"):]
+        remote_type = url_path.split("+")[0]
+        url_path = url_path[len(remote_type)+2:] # +2 to remove both + and {
+        
+        remote_http_url = url_path.split("}")[0]
+        url_path = url_path[len(remote_http_url)+2:] # +2 to remove both } and +
+
+        remote_config = url_path.split("/")[0]
+        url_path = url_path[len(remote_config)+1:] # +1 /
+
+    cache_url_path = os.path.join("r", remote_config, url_path.replace("/", os.sep))
+
+    logger.info(f"Remote Type: {remote_type}, Remote HTTP URL: {remote_http_url}, Remote Name: {remote_config}")
+    # logger.info(f"url_path: {url_path}, cache_url_path: {cache_url_path}")
+
     if url_path == "v1/ping" and len(cached_headers) != 0:
         logger.info(f"Cached headers: {cached_headers}")
         return Response(headers=cached_headers)
@@ -45,18 +70,6 @@ async def get_external_site(request: Request, url_path: str):
     user_agent = request.headers.get('user-agent')
     if not user_agent.startswith("Conan"):
         user_agent = "Conan/1.65.0-dev (Windows 10; Python 3.12.2; AMD64)"
-
-    # Remote Configuration
-    # /r/conan+url+name/
-    remote_type = "conan"
-    remote_types_allowed = ["conan",]
-    remote_http_url = "https://bincrafters.jfrog.io/artifactory/api/conan/conan-legacy-inexorgame/"
-    remote_config = "inexorgame"
-
-    cache_url_path = os.path.join("r", remote_config, url_path.replace("/", os.sep))
-
-    # logger.info(f"Remote Type: {remote_type}, Remote HTTP URL: {remote_http_url}")
-    # logger.info(f"url_path: {url_path}, cache_url_path: {cache_url_path}")
 
     default_response_type = "application/json"
     if url_path.endswith(".txt") or url_path.endswith(".py"):
@@ -118,14 +131,22 @@ def _shell_background(command: str) -> None:
     return None
 
 if __name__ == "__main__":
-    _PORT = 8086
+    parser = argparse.ArgumentParser(description="Generate a static Conan remote from an existing Conan remote.")
+    parser.add_argument("--port", type=int, default=8042, help="Port to run the FastAPI server.")
+    parser.add_argument("--remote-url", type=str, default="https://bincrafters.jfrog.io/artifactory/api/conan/conan-legacy-inexorgame/", help="Remote URL to generate the static remote from. It can not contain spaces or plus signs (+) and has to end on a slash.")
+    parser.add_argument("--remote-name", type=str, default="inexorgame", help="Remote URL to generate the static remote from.")
+    args = parser.parse_args()
+
+    _PORT = args.port
+    # remote_url_quoted = urllib.parse.quote(args.remote_url, safe="")
+    remote_url_quoted = "{" + args.remote_url + "}"
 
     # uvicorn.run(app, host="0.0.0.0", port=_PORT, log_level="info")
     _shell_background(f"fastapi dev generate.py --port {_PORT} --no-reload")
     time.sleep(3)
 
     _shell("conan remote remove bincrafters-remote-tmp || true", check=False)
-    _shell(f"conan remote add bincrafters-remote-tmp http://127.0.0.1:{_PORT}/")
+    _shell(f"conan remote add bincrafters-remote-tmp http://127.0.0.1:{_PORT}/r/conan+{remote_url_quoted}+{args.remote_name}/")
     _shell("conan remote list")
 
     references = _shell("conan search '*' -r bincrafters-remote-tmp --raw --case-sensitive")
