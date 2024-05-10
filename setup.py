c655fc35c02e97cb3174d5d03b9496912c24a115
Croydon <git@cr0ydon.com>
2024-05-10 15:30:55 +0200

    Implement CD

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1",
+    version="0.1.0",
 
     description="Run locally a Conan remote that builds open static files.",
     long_description="This tool is something like a locally run proxy server to provide the Conan client with a Conan remote as it expects, building upon static remote text files.",
