dad0319216f9d56b39aacb6da7e0aa71a5c195f2
Croydon <git@cr0ydon.com>
2024-05-10 12:04:17 +0200

    Helpers: Fix: Follow redirects again

--- bincrafters_conan_remote/helpers.py
+++ bincrafters_conan_remote/helpers.py
@@ -12,7 +12,7 @@ conf = {
     "user_agent_default": "Conan/1.65.0-dev (Windows 10; Python 3.12.2; AMD64)",
 }
 
-def make_request(getting_url: str, user_agent: str):
-    with httpx.Client() as client:
+def make_request(getting_url: str, user_agent: str, follow_redirects: bool = True):
+    with httpx.Client(follow_redirects=follow_redirects) as client:
         logger.info(f"Getting URL: {getting_url}")
         return client.get(getting_url, headers={'User-Agent': user_agent})
