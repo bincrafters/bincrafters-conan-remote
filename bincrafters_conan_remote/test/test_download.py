5eb24b51c5dd64e7ea8611eb2020156dfc5edcea
Croydon <git@cr0ydon.com>
2024-05-18 04:11:46 +0200

    Tests: Add more download tests

--- bincrafters_conan_remote/test/test_download.py
+++ bincrafters_conan_remote/test/test_download.py
@@ -1,13 +1,30 @@
 from bincrafters_conan_remote.test.helpers import cli_command
 
 import pytest
-import time
+
 
 def test_download_revision_latest():
-    print("Sleeping for 25 seconds...")
-    time.sleep(25)
     cli_command(
         ["conan", "download", "-r", "inexorgame", "--recipe", "InexorGlueGen/0.6.11@inexorgame/stable"],
+        expected_outputs=[
+            "Downloading conanmanifest.txt",
+            "Downloading conanfile.py",
+            "Downloading conan_sources.tgz"]
+        )
+
+def test_download_revision_specific():
+    cli_command(
+        ["conan", "download", "-r", "inexorgame", "--recipe", "grpc/1.34.1@inexorgame/stable#0"],
+        expected_outputs=[
+            "Downloading conanmanifest.txt",
+            "Downloading conanfile.py",
+            "Downloading conan_sources.tgz"]
+        )
+
+
+def test_download_no_explicit_remote():
+    cli_command(
+        ["conan", "download", "--recipe", "v8/7.6.66@inexorgame/testing"],
         expected_outputs=[
             "Downloading conanmanifest.txt",
             "Downloading conanfile.py",
